70eb630acd616fcda9a7dcd05e07ff0da5b2d7a8
Lou Griffith <griffithlou@gmail.com>
2015-06-09 16:07:26 -0500

    Initial Commit

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,9 +12,9 @@ class UserTableSeeder extends Seeder {
 		DB::table(config('auth.table'))->truncate();
 
 		$user = array(
-			'name' => 'Admin Istrator',
-			'email' => 'admin@admin.com',
-			'password' => bcrypt('1234'),
+			'name' => 'Lou Griffith',
+			'email' => 'lou@spottsfield.com',
+			'password' => 'Megatron20$$',
 			'confirmation_code' => md5(uniqid(mt_rand(), true)),
 			'confirmed' => true,
 			'created_at' => Carbon::now(),
